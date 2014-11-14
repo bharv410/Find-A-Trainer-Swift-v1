c69d1858385de156cb1981f39ecead9491b9b0bb
Panafold <panafold@kit-halvorsens-imac.local>
2014-11-14 12:07:30 -0800

    Added photo choose

--- Find-A-Trainer/TrainerSignUpViewController.swift
+++ Find-A-Trainer/TrainerSignUpViewController.swift
@@ -9,8 +9,10 @@
 import Foundation
 import UIKit
 
-class TrainerSignUpViewController: UIViewController {
-    
+class TrainerSignUpViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var chooseBuuton: UIButton!
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,29 @@ class TrainerSignUpViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in
+            
+        })
+        
+        imageView.image = image
+    }
+    
+    @IBAction func btnClicked(){
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+            println("Button capture")
+            
+            
+            imagePicker.delegate = self;
+            imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
     }
 }
 

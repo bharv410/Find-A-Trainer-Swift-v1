588977a09dcba28f1888783acccdd32a93d67a75
Panafold <panafold@kit-halvorsens-imac.local>
2014-11-14 10:34:29 -0800

    Added icon and home screen

---
+++ Find-A-Trainer/TrainerSignUpViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TrainerSignUpViewController.swift
+//  Find-A-Trainer
+//
+//  Created by Panafold on 11/13/14.
+//  Copyright (c) 2014 Panafold. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrainerSignUpViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("worked");
+        self.navigationController?.navigationBarHidden=false;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
--- Find-A-Trainer/ViewController.swift
+++ Find-A-Trainer/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("hi")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func findLocalTrainers(sender : AnyObject) {
+        let alertController = UIAlertController(title: "Trainee Sign up", message:
+            "Get ready to find premier trainers", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 }
 
